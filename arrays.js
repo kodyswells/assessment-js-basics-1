02a8bc5a26d2c58485191aeafa16808148048108
Kody Wells <kodyswells@gmail.com>
2024-01-16 19:35:34 -0700

    Solved The Arrays Section

--- arrays.js
+++ arrays.js
@@ -3,12 +3,12 @@
 
 const faveColors = ['red', 'green', 'black']
 
-//CODE HERE
+let colorCopy = faveColors.slice(0,3);
 
 //////////////////PROBLEM 2////////////////////
 /* Add a fourth color to the end of the 'colorCopy' array using the push method. */
 
-//CODE HERE
+colorCopy.push('Orange');
 
 //////////////////PROBLEM 3////////////////////
 /*
@@ -18,7 +18,7 @@ const faveColors = ['red', 'green', 'black']
 
 const numbers = [1, 2, 3, 4, 5]
 
-//CODE HERE
+let middleNums = numbers.slice(1,4);
 
 
 //////////////////PROBLEM 4////////////////////
@@ -33,4 +33,13 @@ const numbers = [1, 2, 3, 4, 5]
 
 const bigOrSmallArray = [1, 99, 42, 69, 102, -10, 159, 352]
 
-// CODE HERE
+const answers = [];
+
+for(let i = 0; i <= bigOrSmallArray.length; i++) {
+  let filter = bigOrSmallArray[i];
+  if(filter > 100) {
+    answers.push('big');
+  } else if(filter <= 100) {
+    answers.push('small');
+  }
+}
